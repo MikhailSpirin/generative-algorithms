10e14f0376ec5530245e035303660fa6686fcec7
Mikhail Spirin <mikhails@wix.com>
2023-08-26 21:22:09 +0300

    palettes

--- main.py
+++ main.py
@@ -2,24 +2,16 @@ import math
 import random
 import logging
 from shapely.geometry import Point, LineString
+from shapely import affinity
 from PIL import Image, ImageDraw
+from palettes import PALETTES
 
 c = 1.3
-N = 3000
-R0 = 400
+N = 1000
+R0 = 300
 XMAX = 2000
 YMAX = 1500
-
-
-def get_random_color_from_palette():
-    return random.choice(["#c9cca1",
-                          "#caa05a",
-                          "#ae6a47",
-                          "#8b4049",
-                          "#543344",
-                          "#515262",
-                          "#63787d",
-                          "#8ea091"])
+MAX_TRIES = 100
 
 
 def get_all_shapes_params(c, N, R0):
@@ -35,24 +27,17 @@ def get_all_shapes_params(c, N, R0):
 
 
 def get_placement_coords(x_max, y_max):
-    return random.random() * x_max, random.random() * y_max
+    return random.random() * x_max, random.random() * y_max // 100 * 100
 
 
-def overlaps(trial, objs, border, placed):
-    if not trial.intersection(border).is_empty:
+def overlaps(trial, border, placed):
+    if not trial.intersection(border).is_empty or not trial.intersection(placed).is_empty:
         return True
-    if not placed.intersection(trial).is_empty:
-        return True
-
-
-    # for item in objs.values():
-    #     if math.sqrt((x - item['x']) ** 2 + (y - item['y']) ** 2) <= r + item['radius']:
-    #         return True
     return False
 
 
-def draw_object(draw, x, y, r):
-    draw.ellipse((x - r, y - r, x + r, y + r), fill=get_random_color_from_palette(), outline=None, width=1)
+def draw_object(draw, x, y, r, palette):
+    draw.ellipse((x - r, y - r, x + r, y + r), fill=random.choice(palette), outline=None, width=1)
 
 
 def init_canvas():
@@ -61,29 +46,29 @@ def init_canvas():
     return im, draw
 
 
+def create_border():
+    return LineString([[0, 0], [XMAX, 0], [XMAX, YMAX], [0, YMAX], [0, 1]])
+
+
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
 
-    MAX_TRIES = 100
-    placed_objects = {}
     objects_to_place = get_all_shapes_params(c, N, R0)
     image, draw = init_canvas()
-
+    current_palette = random.choice(list(PALETTES.values()))
+    border = create_border()
     tries_logger = []
-    border = LineString([[0, 0], [XMAX, 0], [XMAX, YMAX], [0, YMAX], [0, 1]])
     placed_objects_union = Point(0, 0)
     for i in range(1, N + 1):
         tries = 0
         while tries < MAX_TRIES:
             x, y = get_placement_coords(XMAX, YMAX)
-            trial = Point(x, y).buffer(objects_to_place[i]['radius'])
-            if not overlaps(trial, placed_objects, border, placed_objects_union):
-                draw_object(draw, x, y, objects_to_place[i]['radius'])
-                placed_objects[i] = objects_to_place[i]
-                placed_objects[i]['x'] = x
-                placed_objects[i]['y'] = y
+            radius = objects_to_place[i]['radius']
+            trial = Point(x, y).buffer(radius)
+            if not overlaps(trial, border, placed_objects_union):
+                draw_object(draw, x, y, radius, current_palette)
                 placed_objects_union = placed_objects_union.union(trial)
                 break
             tries += 1
